d796921e0931f334016e298c6bca9e86fcc21fbc
10169 <xu.yuanfeng@datasw.com>
2022-04-01 18:01:56 +0800

    template processor project created

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # nifi
 nifi相关开发
-#从模板创建
+#从模板创建nifi组件项目
 mvn archetype:generate -DarchetypeGroupId=org.apache.nifi -DarchetypeArtifactId=nifi-processor-bundle-archetype
---
+++ nifi-nar-bundles/nifi-template-bundle/nifi-template-processor/src/main/com/xugg001/nifi/template/processors/TemplateProcessor.java
@@ -0,0 +1,19 @@
+package com.xugg001.nifi.template.processors;
+
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.exception.ProcessException;
+
+/**
+ * @author xuyuanfeng169
+ * @date 2021/10/21 18:28
+ */
+public class TemplateProcessor extends AbstractProcessor {
+
+    @Override
+    public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
+
+    }
+
+}
